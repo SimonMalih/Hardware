aadd0ac3f31dbde036470f8dced426d0fe37cf68
Simon Malih <simon.malih@gmail.com>
2023-02-11 21:49:39 +0000

    add repo header

---
+++ Repo.h
@@ -0,0 +1,16 @@
+#ifndef REPO_H_
+#define REPO_H_
+
+#include "Device.h"
+#include "Formatter.h"
+#include "LightBulb.h"
+#include "PWMLED.h"
+#include "Temperature.h"
+#include "Database.h"
+#include "GlobalSettings.h"
+#include "CustomAlarm.h"
+#include "Network.h"
+#include "AuthManager.h"
+#include "LCDDisplay.h"
+
+#endif
